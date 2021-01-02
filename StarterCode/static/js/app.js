cd6627f1c426778481cffee7e33972eefa337c6a
smuktar <limodirectphilly@gmail.com>
2021-01-01 21:05:19 -0500

    getting the table created show on the webpage

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,11 +6,18 @@ var tbody = d3.select("tbody");
 
 //loop through the data and console.log for each ufo sighted report object
 data.forEach((ufoSightings) => {
-    console.log(ufoSightings);
+console.log(ufoSightings);
 //appending one table row "tr"element for each ufo sight report object
-    var row = tbody.append("tr");
-    Object.defineProperties(ufoSightings).forEach(([key, value]) =>{
-        console.log(key, value);
-    })
+var row = tbody.append("tr");
+//use 'object.entries' to console.log for each sight report value
+Object.entries(ufoSightings).forEach(([key, value]) =>{
+console.log(key, value);
+// append a cell 'td' element to the row for each value
+var cell = row.append ("td");
+        
+cell.text(value);
+
+
+    });
    
-})
+});
