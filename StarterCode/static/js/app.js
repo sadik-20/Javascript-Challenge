f6ee4bbe7bb1e89b1f42b02b92d7f9bb9c92bf97
smuktar <limodirectphilly@gmail.com>
2021-01-01 20:51:33 -0500

    appending 'tr' element for each sight

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,9 +4,13 @@ var tableData = data;
 //a refernce to the table body
 var tbody = d3.select("tbody");
 
-//loop through the data and console.log for earch ufo sighted report object
+//loop through the data and console.log for each ufo sighted report object
 data.forEach((ufoSightings) => {
     console.log(ufoSightings);
+//appending one table row "tr"element for each ufo sight report object
     var row = tbody.append("tr");
+    Object.defineProperties(ufoSightings).forEach(([key, value]) =>{
+        console.log(key, value);
+    })
    
-}
+})
